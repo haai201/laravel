4d7059ffd1cc6cb1c2bfc2cdf45c450ef2c222c4
Dương Văn Nhã <nhaduongvan5@gmail.com>
2021-07-26 09:41:52 +0700

    dvnha

--- routes/web.php
+++ routes/web.php
@@ -139,6 +139,9 @@ Route::prefix('admin')->group(function () {
         ]); 
     });
 });
+// Route::group(['prefix' => 'laravel-filemanager', 'middleware' => ['web', 'auth']], function () {
+//     \UniSharp\LaravelFilemanager\Lfm::routes();
+// });
 
 
     
