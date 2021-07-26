067167e688517d52a18d7149e83b75d9d71bb977
Dương Văn Nhã <nhaduongvan5@gmail.com>
2021-07-26 10:20:29 +0700

    dvnha

--- resources/views/admin/product/add.blade.php
+++ resources/views/admin/product/add.blade.php
@@ -62,16 +62,17 @@
 <label>Nhập tag cho sản phẩm</label>
 <select name="tags[]" class="form-control tags_select_choose" multiple="multiple">
 </select></div>
-<div class="form-group">
-    <label >Nhập nội dung</label>
-    <textarea class="form-control" name="content" rows="3"></textarea>
-  </div>
-  
-  <button type="submit" class="btn btn-primary float-right">Xác nhận</button>
-</form>
-</div>
+
           <!-- /.col-md-6 -->
         </div>
+        <div class="form-group col-md-12">
+    <label >Nhập nội dung</label>
+    <textarea name="content" class="form-control tinymce_editor_init"></textarea>
+  </div>
+  <div class="col-md-12">
+  <button type="submit" class="btn btn-primary float-right">Xác nhận</button></div>
+</form>
+</div>
         <!-- /.row -->
       </div><!-- /.container-fluid -->
     </div>
@@ -82,16 +83,44 @@
 
 @section('js')
 <script src="{{ asset('vendors/select2/select2.min.js')}}"></script>
-<script>
-  $(".tags_select_choose").select2({
-    tags: true,
-    tokenSeparators: [',', ' ']
-  })
-  $(".select2_init").select2({
-    placeholder: "Chọn danh mục",
-    allowClear: true
-  })
-    
+<!-- <script src="//cdn.tinymce.com/4/tinymce.min.js"></script> -->
+<script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js"></script>
 
+<script>
+  var editor_config = {
+    path_absolute : "/",
+    selector: "textarea.tinymce_editor_init",
+    plugins: [
+      "advlist autolink lists link image charmap print preview hr anchor pagebreak",
+      "searchreplace wordcount visualblocks visualchars code fullscreen",
+      "insertdatetime media nonbreaking save table contextmenu directionality",
+      "emoticons template paste textcolor colorpicker textpattern"
+    ],
+    toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media",
+    relative_urls: false,
+    file_browser_callback : function(field_name, url, type, win) {
+      var x = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
+      var y = window.innerHeight|| document.documentElement.clientHeight|| document.getElementsByTagName('body')[0].clientHeight;
+
+      var cmsURL = editor_config.path_absolute + 'laravel-filemanager?field_name=' + field_name;
+      if (type == 'image') {
+        cmsURL = cmsURL + "&type=Images";
+      } else {
+        cmsURL = cmsURL + "&type=Files";
+      }
+
+      tinyMCE.activeEditor.windowManager.open({
+        file : cmsURL,
+        title : 'Filemanager',
+        width : x * 0.8,
+        height : y * 0.8,
+        resizable : "yes",
+        close_previous : "no"
+      });
+    }
+  };
+
+  tinymce.init(editor_config);
 </script>
+
 @endsection
