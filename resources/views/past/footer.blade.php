f8c972b9ec85243432ee2c1ed8134d5682a22e4b
Nha <nhaduongvan5@gmail.com>
2021-08-14 14:45:48 +0700

    dvnha commit

--- resources/views/past/footer.blade.php
+++
@@ -1,8 +0,0 @@
-<footer class="main-footer">
-    <!-- To the right -->
-    <div class="float-right d-none d-sm-inline">
-      Anything you want
-    </div>
-    <!-- Default to the left -->
-    <strong>Dương Văn Nhã.</strong> All rights reserved.  <strong>02/02/2001</strong>
-  </footer>
